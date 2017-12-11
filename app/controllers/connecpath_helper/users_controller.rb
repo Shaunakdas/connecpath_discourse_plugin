0ef3889cb5efbb2c9d1675c541dcae63a61e07d9
Shaunak Das <shaunakdas2020@gmail.com>
2017-12-11 09:43:02 +0530

    Removed all trial methods for Forgot Password

--- app/controllers/connecpath_helper/users_controller.rb
+++ app/controllers/connecpath_helper/users_controller.rb
@@ -9,48 +9,6 @@ module ConnecpathHelper
     #   Rails.application.routes.call(request_env)
     # end
 
-    def trial_call
-      render json: {status: 'found'}
-    end
-
-    def trial_user_1
-      user = User.find_by_username_or_email(params[:login])
-      user_presence = user.present? && user.id > 0 && !user.staged
-      if user_presence
-        email_token = user.email_tokens.create(email: user.email)
-      end
-      render json: {status: user}
-    end
-
-
-    def trial_user_2
-      user = User.find_by_username_or_email(params[:login])
-      user_presence = user.present? && user.id > 0 && !user.staged
-      if user_presence
-        email_token = user.email_tokens.create(email: user.email)
-        Jobs.enqueue(:critical_user_email, type: :forgot_password, user_id: user.id, email_token: email_token.token)
-      end
-
-      json = { result: "ok", email_token: email_token }
-
-      render json: json
-    end
-
-    def trial_user_3
-      user = User.find_by_username_or_email(params[:login])
-      user_presence = user.present? && user.id > 0 && !user.staged
-      if user_presence
-        email_token = user.email_tokens.create(email: user.email)
-      end
-
-      json = { result: "ok" }
-      unless SiteSetting.forgot_password_strict
-        json[:user_found] = user_presence
-      end
-
-      render json: json
-    end
-
     def forgot_password
       user = User.find_by_username_or_email(params[:login])
       user_presence = user.present? && user.id > 0 && !user.staged
@@ -60,9 +18,6 @@ module ConnecpathHelper
       end
 
       json = { result: "ok" }
-      unless SiteSetting.forgot_password_strict
-        json[:user_found] = user_presence
-      end
 
       render json: json
     end
